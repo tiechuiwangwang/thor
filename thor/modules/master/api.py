2a42452eb26fe256578d2d437760e86f476c54e4
shonenada <shonenada@gmail.com>
2019-03-02 13:46:29 +0800

    chore: rename from 'index' to 'master'

--- thor/modules/master/api.py
+++ thor/modules/master/api.py
@@ -3,7 +3,7 @@ from flask import jsonify
 from thor.blueprint import create_api_blueprint
 
 
-bp = create_api_blueprint('index', __name__)
+bp = create_api_blueprint('master', __name__)
 
 
 @bp.route('/', methods=['GET', 'POST'])
